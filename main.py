8fd4342fbb34797bc0cef276994ee10191fad8af
ku4ka778 <not.found.unknow10@gmail.com>
2026-01-29 00:09:23 +0300

    kill_me_pls

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
+from bs4 import BeautifulSoup as bs
+import random
+
+def creating_link():
+    get_rand_id = random.randint(1111,9999)
+    url = f"https://ru.dotabuff.com/matches/866768{get_rand_id}"
+    return str(url)
+
+# def parsing(url):
+def parsing():
+    url = "https://ru.dotabuff.com/matches/8667684671"
+    r = requests.get(url)
+    soup = bs(r.text, "html.parser")
+    players_sheet = soup.find(class_="team-results")
+    print(players_sheet)
 
 
-
-def main():
-    print("https://stratz.com/matches/8667685544/builds")
 if __name__ == "__main__":
-    main()
+    # parsing(creating_link())
+    parsing()
