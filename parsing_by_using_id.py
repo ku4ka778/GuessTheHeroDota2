dba1c33df01c433f7e7886299da100a2e8074dc6
ku4ka778 <not.found.unknow10@gmail.com>
2026-02-03 17:26:03 +0300

    9_FixedSeleniumAndSomeFixes

--- parsing_by_using_id.py
+++ parsing_by_using_id.py
@@ -1,46 +1,40 @@
-from random import randint
-from parsing_id import sorting_html_code
+from parsing_id import creating_links
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import re
 import shutil
-import os
 
-def gen_link_to_match(id):
-    game_id = sorting_html_code()
-    print(len(game_id), "ready to be parsed")
-    game_id = game_id[id][9:]
-    url = f"https://www.opendota.com/matches/{game_id}"
-    print(url)
-    return url
 
-def selenium(url):
+def selenium(i):
+    url = creating_links()
+    print(url[i])
     print("Parsing...")
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-    driver.get(url)
+    driver.get(url[i])
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     try:
         time.sleep(10)
         html_source = driver.page_source
-
         with open("html_code.txt", "w", encoding='utf-8') as f:
             f.write(html_source)
         f.close()
         print("Html source saved successfully")
-    except:
+    except Exception as e:
+        print(e)
         print("Error")
     finally:
         driver.quit()
     return driver
 
-def get_match_lenght():
+def get_match_length():
     with open("html_code.txt", 'r') as f:
         text = f.read()
         search_id = r"<div><span>\d+:\d+"
         result = re.findall(search_id, text)
+        print(result)
         game_time = result[0][11:]
         print(game_time)
     f.close()
@@ -103,28 +97,28 @@ def neutral_item():
     f.close()
     return neutral_items
 
-def agan_shard():
+def agan():
     with open("html_code.txt", 'r') as f:
         text = f.read()
         search_id = r'data-for="scepter" currentitem="\w+'
         agan = re.findall(search_id, text)
         agan = [agan[32:] for agan in agan]
+        print(agan)
+    f.close()
+    return agan
+
+def shard():
+    with open("html_code.txt", 'r') as f:
+        text = f.read()
         search_id = r'data-for="shard" currentitem="\w+'
         shard = re.findall(search_id, text)
         shard = [shard[30:] for shard in shard]
-        print(agan, "\n",  shard)
     f.close()
-    return agan,shard
+    return shard
 
-def creating_full_heros_info(match_id, heroes, items, neutral, agan, shard):
-    os.remove("html_code(replaced).txt")
-    os.remove("html_code.txt")
-    heroes_inf = {}
-    #{hero, [items], neutral, agan, shard}
+def creating_full_heros_info():
+    pass
 
-selenium(gen_link_to_match(0))
-get_match_lenght()
-characters()
-characters_items()
-neutral_item()
-agan_shard()
+
+
+
